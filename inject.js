2fcf252d7ea7795b8ec3f2ef486bfa5d48600078
daniel1984 <aciddaniel@gmail.com>
2018-03-12 02:13:10 +0200

    attempt to add UI for saving refs

--- inject.js
+++ inject.js
@@ -1,76 +1,120 @@
-window.wpsEnabled = false;
+(function wps() {
+  window.wpsEnabled = false;
 
-let initialBackgroundColor;
-let initialCursor;
+  var initialBackgroundColor;
+  var initialCursor;
 
-document.querySelectorAll('body *').forEach((el) => {
-  el.addEventListener('mouseover', (e) => {
-    if (!window.wpsEnabled) {
-      return true;
-    }
-
-    e.preventDefault();
-    e.stopPropagation();
-
-    const elStyles = window.getComputedStyle(e.target);
-    initialBackgroundColor = elStyles.backgroundColor;
-    initialCursor = elStyles.cursor;
-    e.target.style.backgroundColor = 'rgba(255, 100, 50, .3)';
-    e.target.style.cursor = 'pointer';
-  });
-
-  el.addEventListener('mouseout', (e) => {
-    if (!window.wpsEnabled) {
-      return true;
-    }
-
-    e.target.style.backgroundColor = initialBackgroundColor;
-    e.target.style.cursor = initialCursor;
-  });
-
-  function getPath(el, path) {
-    path = path.length ? path : [];
-
-    const tagName = el.tagName.toLowerCase();
-    const elClass = el.className ? `.${Array.from(el.classList).join('.')}` : '';
-    const elId = el.id ? `#${el.id}` : '';
-    const selector = elId || elClass;
-
-    if (el.parentElement && el.parentElement.children.length) {
-      const elementIndex = Array.from(el.parentElement.children).indexOf(el);
-      path.unshift(`${tagName}${selector}:nth-child(${elementIndex + 1})`);
-    } else {
-      path.unshift(`${tagName}${selector}:nth-child(0)`);
-    }
-
-    if (el.parentElement.nodeName.toLowerCase() !== 'html') {
-      return getPath(el.parentElement, path);
-    }
-
-    return path;
+  function getPopupContainer() {
+    const popupContainer = document.createElement('div');
+    popupContainer.style.position = 'fixed';
+    popupContainer.style.zIndex = '3000';
+    popupContainer.style.top = '0';
+    popupContainer.style.left = '0';
+    popupContainer.style.right = '0';
+    popupContainer.style.bottom = '0';
+    popupContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+    return popupContainer;
   }
 
-  el.addEventListener('click', (e) => {
-    if (!window.wpsEnabled) {
-      return true;
+  function getPopup() {
+    const popup = document.createElement('div');
+    popup.style.minWidth = '500px';
+    popup.style.minHeight = '300px';
+    popup.style.backgroundColor = '#fff';
+    popup.style.position = 'absolute';
+    popup.style.top = '50%';
+    popup.style.left = '50%';
+    popup.style.transform = 'translate(-50%, -50%)';
+    return popup;
+  }
+
+  function getButton({ state, cb, text }) {
+    const button = document.createElement('button');
+    button.style.minWidth = '500px';
+    button.style.minHeight = '300px';
+    button.style.backgroundColor = '#fff';
+    button.style.position = 'absolute';
+    button.style.top = '50%';
+    button.style.left = '50%';
+    button.style.transform = 'translate(-50%, -50%)';
+    return button;
+  }
+
+  document.querySelectorAll('body *').forEach((el) => {
+    el.addEventListener('mouseover', (e) => {
+      if (!window.wpsEnabled) {
+        return true;
+      }
+
+      e.preventDefault();
+      e.stopPropagation();
+
+      const elStyles = window.getComputedStyle(e.target);
+      initialBackgroundColor = elStyles.backgroundColor;
+      initialCursor = elStyles.cursor;
+      e.target.style.backgroundColor = 'rgba(255, 100, 50, .3)';
+      e.target.style.cursor = 'pointer';
+    });
+
+    el.addEventListener('mouseout', (e) => {
+      if (!window.wpsEnabled) {
+        return true;
+      }
+
+      e.target.style.backgroundColor = initialBackgroundColor;
+      e.target.style.cursor = initialCursor;
+    });
+
+    function getPath(el, path) {
+      path = path.length ? path : [];
+
+      const tagName = el.tagName.toLowerCase();
+      const elClass = el.className ? `.${Array.from(el.classList).join('.')}` : '';
+      const elId = el.id ? `#${el.id}` : '';
+      const selector = elId || elClass;
+
+      if (el.parentElement && el.parentElement.children.length) {
+        const elementIndex = Array.from(el.parentElement.children).indexOf(el);
+        path.unshift(`${tagName}${selector}:nth-child(${elementIndex + 1})`);
+      } else {
+        path.unshift(`${tagName}${selector}:nth-child(0)`);
+      }
+
+      if (el.parentElement.nodeName.toLowerCase() !== 'html') {
+        return getPath(el.parentElement, path);
+      }
+
+      return path;
     }
 
-    e.preventDefault();
-    e.stopPropagation();
+    el.addEventListener('click', (e) => {
+      if (!window.wpsEnabled) {
+        return true;
+      }
 
-    const path = getPath(e.currentTarget, []).join(' > ');
-    const el = document.querySelector(path);
+      e.preventDefault();
+      e.stopPropagation();
 
-    const fieldInfo = {
-      path,
-      name: 'testName',
-      textContent: el.textContent,
-      innerHTML: el.innerHTML,
-      innerText: el.innerText,
-    };
+      const path = getPath(e.currentTarget, []).join(' > ');
+      const el = document.querySelector(path);
 
-    chrome.extension.sendMessage({ fn: 'persistFieldInfo', payload: fieldInfo }, () => {
-      console.log("Something happened");
+      const fieldInfo = {
+        path,
+        name: 'testName',
+        textContent: el.textContent,
+        innerHTML: el.innerHTML,
+        innerText: el.innerText,
+      };
+
+      // const popupContainer = getPopupContainer();
+      // const popup = getPopup();
+
+      // popupContainer.appendChild(popup);
+      // document.body.appendChild(popupContainer);
+      // document.body.appendChild(popup);
+
+      chrome.extension.sendMessage({ fn: 'persistFieldInfo', payload: fieldInfo });
     });
   });
-});
+})();
+
--- popup.js
+++ popup.js
@@ -1,21 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('popup DOMContentLoaded');
+  const btnEnable = document.querySelector('.btn-enable');
+  const btnDisable = document.querySelector('.btn-disable');
 
   chrome.runtime.getBackgroundPage((bg) => {
     console.log('popup ', bg.haha, new Date());
   });
 
-  const btnEnable = document.querySelector('.btn-enable');
   btnEnable.addEventListener('click', () => {
-    chrome.runtime.sendMessage({ fn: 'enableScrapeSelections' }, (response) => {
-      console.log('+=================>> enableScrapeSelections ', response);
-    });
+    chrome.runtime.sendMessage({ fn: 'enableScrapeSelections' });
   });
 
-  const btnDisable = document.querySelector('.btn-disable');
   btnDisable.addEventListener('click', () => {
-    chrome.runtime.sendMessage({ fn: 'disableScraper' }, (response) => {
-      console.log('+=================>> reloadTab ', response);
-    });
+    chrome.runtime.sendMessage({ fn: 'disableScraper' });
   });
 });
